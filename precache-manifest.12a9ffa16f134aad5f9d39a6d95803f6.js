28a829c12f09c51f3a96e1088022ad7379eaf2be
Grasu-Lucian <grasu.lucian20@yahoo.com>
2021-11-24 15:50:17 +0200

    Updates

---
+++ precache-manifest.12a9ffa16f134aad5f9d39a6d95803f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "832e60ef87d6a08c4796fa197509f175",
+    "url": "/Dec2Binary-Binary2Dec/index.html"
+  },
+  {
+    "revision": "07f9daad5438e643b4ef",
+    "url": "/Dec2Binary-Binary2Dec/static/css/main.da091882.chunk.css"
+  },
+  {
+    "revision": "c2c20b3be18e0d3ff74c",
+    "url": "/Dec2Binary-Binary2Dec/static/js/2.1476f585.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Dec2Binary-Binary2Dec/static/js/2.1476f585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f9daad5438e643b4ef",
+    "url": "/Dec2Binary-Binary2Dec/static/js/main.a2693975.chunk.js"
+  },
+  {
+    "revision": "8163f5adfd2860b51cdd",
+    "url": "/Dec2Binary-Binary2Dec/static/js/runtime-main.4862d69b.js"
+  }
+]);
